4f81a89ea9251f62fb801e3d1a3cc13bbe47fbd3
ThisWasSparta <floris245@gmail.com>
2024-01-25 17:34:50 +0100

    add graphics for car model
    fixed some typos/grammar errors

--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -6,35 +6,25 @@ export default [
       Combining my knowledge in pixel art with 3D modeling, I created this futuristic looking car.
 
         <div class="paragraph center">
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
+          <img class="pc-screenshot" src="img/projects/car-front-side.png" alt="Car's front" />
+          <img class="pc-screenshot" src="img/projects/car-back-side.png" alt="Car's back" />
+          <img class="pc-screenshot" src="img/projects/car-side-ortho.png" alt="Car's side, orthographic camera" /> 
+          <img class="pc-screenshot" src="img/projects/car-top-ortho.png" alt="Car from above, orthographic camera" /> 
         </div>
     `, "#c10606", false, true),
     new ProjectData("project-7", "2D Animation", "img/projects/knight.gif", `
     <div class="paragraph">
-                Here's some examples of 2D animations I've made, primarily for the top down game discussed in the games section.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@karolina-grabowska">Karolina Grabowska</a>.
+                Here's some examples of 2D animations I've made in Aseprite, primarily for the top down game discussed in the games section.
             </div>
-
-            <div class="paragraph">
-              Main features :
-              <ul>
-              <li>Some stuff</li>
-              <li>Some great stuff</li>
-              <li>More awesome stuff</li>
-              <li>And then some</li>
-              </ul>
-          </div>
 
             <div class="paragraph center">
                 <img class="pc-screenshot" src="img/projects/knight.gif" />
                 <img class="pc-screenshot" src="img/projects/rogue.gif" />
                 <img class="pc-screenshot" src="img/projects/wizard.gif" /> 
             </div>`, "#1ca1e2"),
-    new ProjectData("project-8", "Various Graphics", "img/projects/charactercollage.png", `
+    new ProjectData("project-8", "Various Graphics", "img/projects/charactercollage-export.png", `
     <div class="paragraph">
-                Here's some samples of the spritework I've done in the years I've been learning game development.
+                Here's some samples of the spritework I've created in the years I've been learning game development.
             </div>
         <div class="paragraph center">
           <img class="pc-screenshot" src="img/projects/charactercollage-export.png" alt="Collage of characters. Some original, some not." />
