69d8763b3343d52115771924627fdd981f46a4f0
ThisWasSparta <floris245@gmail.com>
2024-01-25 18:42:58 +0100

    add images for humanoid character

--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -12,30 +12,35 @@ export default [
           <img class="pc-screenshot" src="img/projects/car-top-ortho.png" alt="Car from above, orthographic camera" /> 
         </div>
     `, "#c10606", false, true),
-	new ProjectData("project-9", "Humanoid Character", "i'm/projects/nicholas.png",`
+	new ProjectData("project-9", "Humanoid Character", "img/projects/nicholas.png",`
 	<div class="paragraph">
-	This was my first attempt at creating a humanoid character from scratch.
-	</div>
+	This was my first attempt at creating a humanoid character from scratch. Proper topology for a model like this is paramount if you want it to look good when animating. I swiftly learned that there's a lot of things to know and keep in mind.\n
+	I started with the face, slowly working my way down from there to the character's torso, shoulders, arms and hands.
+		<div class="paragraph center">
+			  <img class="pc-screenshot" src="img/projects/shoulder-topology.png" alt="Car's front" />
+			  <img class="pc-screenshot" src="img/projects/face-topology.png" alt="Car's back" />
+			</div>
+		</div>
 	`),
     new ProjectData("project-7", "2D Animation", "img/projects/knight.gif", `
     <div class="paragraph">
                 Here's some examples of 2D animations I've made in Aseprite, primarily for the top down game discussed in the games section.
-            </div>
+    </div>
 
-            <div class="paragraph center">
-                <img class="pc-screenshot" src="img/projects/knight.gif" />
-                <img class="pc-screenshot" src="img/projects/rogue.gif" />
-                <img class="pc-screenshot" src="img/projects/wizard.gif" /> 
-            </div>`, "#1ca1e2"),
+    <div class="paragraph center">
+		<img class="pc-screenshot" src="img/projects/knight.gif" />
+		<img class="pc-screenshot" src="img/projects/rogue.gif" />
+		<img class="pc-screenshot" src="img/projects/wizard.gif" /> 
+	</div>`, "#1ca1e2"),
     new ProjectData("project-8", "Various Graphics", "img/projects/charactercollage-export.png", `
     <div class="paragraph">
                 Here's some samples of the spritework I've created in the years I've been learning game development.
             </div>
-        <div class="paragraph center">
-          <img class="large-image-collage" src="img/projects/charactercollage-export.png" alt="Collage of characters. Some original, some not." />
-        </div><div class="paragraph center">
-          <img class="large-image-collage" src="img/projects/collageitems-export.png" alt="Collage of item sprites" />
-        </div><div class="paragraph center">
-          <img class="large-image-collage" src="img/projects/collagetileset-export.png" alt="Collage of tileset graphics" /> 
-        </div>`),
+				<div class="paragraph center">
+				  <img class="large-image-collage" src="img/projects/charactercollage-export.png" alt="Collage of characters. Some original, some not." />
+				</div><div class="paragraph center">
+				  <img class="large-image-collage" src="img/projects/collageitems-export.png" alt="Collage of item sprites" />
+				</div><div class="paragraph center">
+				  <img class="large-image-collage" src="img/projects/collagetileset-export.png" alt="Collage of tileset graphics" /> 
+			</div>`),
 ];
